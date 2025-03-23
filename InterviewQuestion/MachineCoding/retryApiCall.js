a4e7b709f35c5a596828af1f1c571cb8b611efd0
sonukumar-trustt <160998790+sonukumar-trustt@users.noreply.github.com>
2025-03-24 01:22:31 +0530

    defined the function to make retry api call

---
+++ InterviewQuestion/MachineCoding/retryApiCall.js
@@ -0,0 +1,22 @@
+async function fetchDataWithRetry(url, retryCount = 3, dealy = 1000) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      if (retryCount === 0) {
+        throw new Error("Retry limit reached");
+      } else {
+        await new Promise((resolve) => setTimeout(resolve, dealy));
+        return fetchDataWithRetry(url, retryCount - 1, dealy*2);
+      }
+    } else {
+      return response.json();
+    }
+  } catch (error) {
+    if(retryCount === 0) {
+      throw new Error("Retry limit reached");
+    }else{
+        await new Promise((resolve) => setTimeout(resolve, dealy));
+        return fetchDataWithRetry(url, retryCount - 1, dealy*2);
+    }
+  }
+}
